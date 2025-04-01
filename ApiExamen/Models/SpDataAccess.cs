718c6b5898d494405801171b6480ab22a80d0e2f
LxIllan <L.Fernando.Illan@gmail.com>
2025-04-01 10:08:43 -0600

    Implement AgregarExamen, ActualizarExamen and EliminarExamen methods in SpDataAccess.

--- ApiExamen/Models/SpDataAccess.cs
+++ ApiExamen/Models/SpDataAccess.cs
@@ -1,16 +1,69 @@
 using ApiExamen.Interfaces;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace ApiExamen.Models;
 
 public class SpDataAccess : IDataAccess
 {
+    private readonly string ConnectionString;
+
+    public SpDataAccess()
+    {
+        ConnectionString = @"Server=NADIA\SQLEXPRESS;Database=BdiExamen;Integrated Security=True;TrustServerCertificate=True;";
+    }
+
     public (bool, string) AgregarExamen(int Id, string Nombre, string Descripcion)
     {
-        throw new NotImplementedException();
+        bool isCreated = false;
+        string message = "";
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand("spAgregar", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int) { Value = Id });
+                cmd.Parameters.Add(new SqlParameter("@Nombre", SqlDbType.VarChar) { Value = Nombre });
+                cmd.Parameters.Add(new SqlParameter("@Descripcion", SqlDbType.VarChar) { Value = Descripcion });
+
+                connection.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        isCreated = (int)reader["Code"] == 0;
+                        message = (string)reader["Message"];
+                    }
+                }
+                connection.Close();
+            }
+        }
+        return (isCreated, message);
     }
     public bool ActualizarExamen(int Id, string Nombre, string Descripcion)
     {
-        throw new NotImplementedException();
+        bool isUpdated = false;
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand("spActualizar", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int) { Value = Id });
+                cmd.Parameters.Add(new SqlParameter("@Nombre", SqlDbType.VarChar) { Value = Nombre });
+                cmd.Parameters.Add(new SqlParameter("@Descripcion", SqlDbType.VarChar) { Value = Descripcion });
+
+                connection.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        isUpdated = (int)reader["Code"] == 0;
+                    }
+                }
+                connection.Close();
+            }
+        }
+        return isUpdated;
     }
 
     public bool ConsultarExamen(int Id, string Nombre, string Descripcion)
@@ -20,7 +73,26 @@ public class SpDataAccess : IDataAccess
 
     public bool EliminarExamen(int Id)
     {
-        throw new NotImplementedException();
+        bool isDeleted = false;
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand("spEliminar", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int) { Value = Id });
+
+                connection.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        isDeleted = (int)reader["Code"] == 0;
+                    }
+                }
+                connection.Close();
+            }
+        }
+        return isDeleted;
     }
 
     public void printInstance()
