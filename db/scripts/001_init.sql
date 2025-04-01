f84ea2382cf48d2aa3da845e723e5ddef989ffd6
LxIllan <L.Fernando.Illan@gmail.com>
2025-04-01 11:42:01 -0600

    Fix: Set auto incrementable id in tblExamen.

--- db/scripts/001_init.sql
+++ db/scripts/001_init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE BdiExamen;
 
 CREATE TABLE tblExamen
 (
-    idExamen INT NOT NULL PRIMARY KEY,
+    idExamen INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     Nombre VARCHAR(255),
     Descripcion VARCHAR(255)
 );
