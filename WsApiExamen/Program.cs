2ba750bd7d8bc26a75968c98e3f2c1b3a9ad1b27
LxIllan <L.Fernando.Illan@gmail.com>
2025-04-01 11:58:31 -0600

    Config database connection.

--- WsApiExamen/Program.cs
+++ WsApiExamen/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using WsApiexamen.Data;
+using WsApiexamen.Interfaces;
+using WsApiexamen.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IExamService, ExamService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
