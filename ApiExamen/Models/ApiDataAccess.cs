c9a2dc7e1199a9b24475fdb7eb337cdaec7e4ee3
LxIllan <L.Fernando.Illan@gmail.com>
2025-04-01 14:52:12 -0600

    Implement ApiDataAccess.

--- ApiExamen/Models/ApiDataAccess.cs
+++ ApiExamen/Models/ApiDataAccess.cs
@@ -1,26 +1,54 @@
+using System.Text;
+using System.Text.Json;
 using ApiExamen.Interfaces;
 
 namespace ApiExamen.Models;
 
 public class ApiDataAccess : IDataAccess
 {
+    private HttpClient httpClient;
+
+    public ApiDataAccess()
+    {
+        httpClient = new HttpClient();
+    }
+
     public (bool, string) AgregarExamen(int Id, string Nombre, string Descripcion)
     {
-        throw new NotImplementedException();
+        string json = JsonSerializer.Serialize(new { Nombre, Descripcion });
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = Task.Run(() => httpClient.PostAsync("http://localhost:5199/api/exam", content)).Result;
+        string message = response.IsSuccessStatusCode ? "Registro insertado satisfactoriamente" : "Error al insertar en base de datos.";
+        return (response.IsSuccessStatusCode, message);
     }
+
     public bool ActualizarExamen(int Id, string Nombre, string Descripcion)
     {
-        throw new NotImplementedException();
+        string json = JsonSerializer.Serialize(new { Nombre, Descripcion });
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = Task.Run(() => httpClient.PutAsync($"http://localhost:5199/api/exam/{Id}", content)).Result;
+        return response.IsSuccessStatusCode;
     }
 
     public List<Examen> ConsultarExamen(int Id, string Nombre, string Descripcion)
     {
-        throw new NotImplementedException();
+        List<Examen>? exams = new List<Examen>();
+        var response = Task.Run(() => httpClient.GetAsync("http://localhost:5199/api/exam")).Result;
+
+        if (response.IsSuccessStatusCode)
+        {
+            var json = Task.Run(() => response.Content.ReadAsStringAsync()).Result;
+            exams = JsonSerializer.Deserialize<List<Examen>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+        return exams ?? new List<Examen>();
     }
 
     public bool EliminarExamen(int Id)
     {
-        throw new NotImplementedException();
+        var response = Task.Run(() => httpClient.DeleteAsync($"http://localhost:5199/api/exam/{Id}")).Result;
+        return response.IsSuccessStatusCode;
     }
 
     public void printInstance()
